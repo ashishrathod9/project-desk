f1a1145014c45ebb39127ab5c1a1d82ad719f5f1
ashishrathod9 <vatsal.ranpariya04@gmail.com>
2024-12-19 17:58:08 +0530

    Update card.jsx

--- fronted/src/components/ui/card.jsx
+++ fronted/src/components/ui/card.jsx
@@ -48,3 +48,6 @@ const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
 CardFooter.displayName = "CardFooter"
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+
+
+
