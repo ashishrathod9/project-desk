36a3c662c18ecc272613eb723d6cffd65b756fa3
ashishrathod9 <133087879+ashishrathod9@users.noreply.github.com>
2025-02-21 01:49:44 +0530

    commit4

--- backend/src/main/java/org/example/project/Project.java
+++ backend/src/main/java/org/example/project/Project.java
@@ -19,9 +19,12 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String description;
-    private String category;
+//    private String name;
+//    private String description;
+//    private String category;
+private String Name;
+private String Description;
+private String Category;
 
 
 
