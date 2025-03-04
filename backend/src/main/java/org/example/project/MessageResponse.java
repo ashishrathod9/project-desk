e4baa2c1c79800b5ea1bc2cb96d7cacc4cdd2252
ashishrathod9 <133087879+ashishrathod9@users.noreply.github.com>
2025-03-05 01:04:13 +0530

    d

--- backend/src/main/java/org/example/project/MessageResponse.java
+++ backend/src/main/java/org/example/project/MessageResponse.java
@@ -1,10 +1,87 @@
 package org.example.project;
 
 import lombok.AllArgsConstructor;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @Data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @AllArgsConstructor
 @NoArgsConstructor
 public class MessageResponse {
