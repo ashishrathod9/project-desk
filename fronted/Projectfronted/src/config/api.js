56c5d244a8e4e5385b685b62a62936b4a0b06c8d
ashishrathod9 <133087879+ashishrathod9@users.noreply.github.com>
2025-03-05 23:50:06 +0530

    Deployment changes 2

--- fronted/Projectfronted/src/config/api.js
+++ fronted/Projectfronted/src/config/api.js
@@ -2,6 +2,9 @@ import axios from "axios"
 
 export const API_BASE_URL="http://localhost:8080"
 
+
+//export const API_BASE_URL=""
+
 const api=axios.create({baseURL:API_BASE_URL})
 const jwt=localStorage.getItem("jwt");
 
